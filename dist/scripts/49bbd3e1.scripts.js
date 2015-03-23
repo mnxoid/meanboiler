670252f46a26c899e411dd004c8215c092989893
mnxoid <sergolwow@gmail.com>
2015-03-23 19:41:26 +0200

    Build added

---
+++ dist/scripts/49bbd3e1.scripts.js
@@ -0,0 +1,1 @@
+"use strict";(function(){var a=angular.module("meanApp",[]);a.config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}])}).call(this),angular.module("meanApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
