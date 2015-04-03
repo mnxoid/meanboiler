49aed1ab33fc68bc17d1ccb19d85c421a6bf2794
mnxoid <sergolwow@gmail.com>
2015-04-03 16:53:22 +0300

    Some other edit

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -4,12 +4,12 @@ angular.module('meanApp')
   .controller('SearchCtrl', function ($scope, $rootScope) {
     $rootScope.active = 6;
     $scope.results = [
-      'http://placehold.it/150x200',
-      'http://placehold.it/150x200',
-      'http://placehold.it/150x200',
-      'http://placehold.it/150x200',
-      'http://placehold.it/150x200',
-      'http://placehold.it/150x200'
+      'http://placehold.it/160x200',
+      'http://placehold.it/160x200',
+      'http://placehold.it/160x200',
+      'http://placehold.it/160x200',
+      'http://placehold.it/160x200',
+      'http://placehold.it/160x200'
     ];
 
     $scope.featured = [
