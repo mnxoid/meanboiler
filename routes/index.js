c7e27e6a517714e2d515c2b014fac7df74c2e109
mnxoid <sergolwow@gmail.com>
2015-04-05 14:33:35 +0300

    Minor edit

--- routes/index.js
+++ routes/index.js
@@ -64,7 +64,7 @@ module.exports = function (app) {
         });
     });
 
-    app.get('/', function (req, res, next) {
+    app.get('*', function (req, res, next) {
         res.render('index');
     });
 };
