254068f468d1fba9d007488ca03425baaba2f526
Danylo Mylian <danylo.ukl@gmail.com>
2015-04-10 15:45:28 +0300

    Added new services

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,59 +1,65 @@
 'use strict';
 (function(){
 
-    var app = angular
-      .module('RHalls', ['ui.bootstrap', 'ui.router'])
-      .config(function ($stateProvider, $urlRouterProvider) {
+    var app;
+    app = angular
+        .module('RHalls', ['ui.bootstrap', 'ui.router'])
+        .config(function ($stateProvider, $urlRouterProvider) {
 
-        $urlRouterProvider.otherwise('/404');
+            $urlRouterProvider.otherwise('/404');
 
-        $stateProvider
-          .state('home', {
-            url: '/',
-            templateUrl: 'views/main.html',
-            controller: 'MainCtrl'
-          })
-          .state('todo', {
-            url: '/todo',
-            templateUrl: 'views/todo.html',
-            controller: 'TodoCtrl',
-            resolve: {
-              halls: function($hall) {
-                return $hall.get();
-              }
-            }
+            $stateProvider
+                .state('home', {
+                    url: '/',
+                    templateUrl: 'views/main.html',
+                    controller: 'MainCtrl'
+                })
+                .state('todo', {
+                    url: '/todo',
+                    templateUrl: 'views/todo.html',
+                    controller: 'TodoCtrl',
+                    resolve: {
+                        halls: function ($hall) {
+                            return $hall.get();
+                        }
+                    }
 
-         })
-         .state('404', {
-           url: '/404',
-           templateUrl: 'views/404.html',
-           controller: 'NotFoundCtrl'
-         })
-         .state('test', {
-           url: '/test',
-           templateUrl: 'views/test.html',
-           controller: 'TestCtrl'
-         })
-         .state('search', {
-           url: '/search',
-           templateUrl: 'views/search.html',
-           controller: 'SearchCtrl'
-         })
-         .state('about', {
-           url: '/about',
-           templateUrl: 'views/about.html'
-         })
-         .state('contact', {
-           url: '/contact',
-           templateUrl: 'views/contact.html'
-         })
-    })
-    .run(function($rootScope) {
-        $rootScope.sel = function(a) {
-            if ($rootScope.active === a) {
-                return "active";
+                })
+                .state('404', {
+                    url: '/404',
+                    templateUrl: 'views/404.html',
+                    controller: 'NotFoundCtrl'
+                })
+                .state('test', {
+                    url: '/test',
+                    templateUrl: 'views/test.html',
+                    controller: 'TestCtrl'
+                })
+                .state('search', {
+                    url: '/search',
+                    templateUrl: 'views/search.html',
+                    controller: 'SearchCtrl'
+                })
+                .state('about', {
+                    url: '/about',
+                    templateUrl: 'views/about.html'
+                })
+                .state('contact', {
+                    url: '/contact',
+                    templateUrl: 'views/contact.html'
+                })
+        })
+        .run(function ($rootScope) {
+
+            $rootScope.sel = function (a) {
+                if ($rootScope.active === a) {
+                    return "active";
+                }
             };
-        }
-    });
+            $rootScope.$on('$routeChangeStart', function (event, next, current) {
+                //TODO: add logic to handle protected routes
+            });
 
-}.call(this));
+        });
+
+}());
