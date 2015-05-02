8a9d45d272e2a003f9c17b28a32104d4d84c3e3c
Danylo Mylian <danylo.ukl@gmail.com>
2015-05-02 19:31:55 +0300

    Upadte main view

--- app/scripts/directives/csvg.js
+++ app/scripts/directives/csvg.js
@@ -15,6 +15,14 @@
 angular.module('RHalls')
   .directive('csvg', function() {
     return {
+      link: function(scope, element) {
+        console.log(element[0].childNodes[4]);
+
+          element[0].childNodes[4].style.height = '100px';
+          element[0].childNodes[4].style.width = '100px';
+          element[0].childNodes[4].style.display = 'block';
+          element[0].childNodes[4].style.margin = 'auto';
+      },
       templateUrl: function(elem, attr){
       return attr.src;
     }
