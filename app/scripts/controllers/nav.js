d5b0ddedac56e51200df6e4ad781c791dd965fbd
mnxoid <sergolwow@gmail.com>
2015-05-03 19:00:52 +0300

    Navbar fixed

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -7,6 +7,33 @@ function NavCtrl ($scope, $auth, $session) {
         "avatar": 'Profile',
         "content": "Hello Popover<br /><img src='http://netgon.ru/upload/avatars/default-avatar-male.png'> <br> <a href='#' ng-click='logout()'>Log out</a>"
     };
+    $scope.navlinks = [
+        {
+            nghr: "/",
+            uisr: "home",
+            text: "Home"
+        },
+        {
+            nghr: "/about",
+            uisr: "about",
+            text: "About"
+        },
+        {
+            nghr: "/contact",
+            uisr: "contact",
+            text: "Contact"
+        },
+        {
+            nghr: "/todo",
+            uisr: "todo",
+            text: "Todo"
+        },
+        {
+            nghr: "/test",
+            uisr: "test",
+            text: "Test"
+        }
+    ];
 
     $scope.logged_in = function() {
         return $session.get('authenticated');
