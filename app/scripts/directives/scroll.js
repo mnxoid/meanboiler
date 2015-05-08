405316743b87c3f0e27444f4d98488b8c05ab475
Danylo Mylian <danylo.ukl@gmail.com>
2015-05-08 21:51:04 +0300

    Hiding navigation bar when scrolling down. FIX: toggling navigation on any scroll up/down

--- app/scripts/directives/scroll.js
+++ app/scripts/directives/scroll.js
@@ -1,15 +1,21 @@
 angular.module('RHalls')
     .directive('scroll', function($window) {
         return function(scope, element, attrs) {
+            var initial = 0;
 
-            angular.element($window).bind("scroll", function() {
-                //TODO: on scroll down -> add class, on scroll up -> remove
-                if (this.pageYOffset >= 50) {
+            angular.element($window).bind("scroll", function(e) {
+
+                var current = this.pageYOffset;
+
+                if (current >= initial) {
                     scope.boolChangeClass = true;
                 } else {
                     scope.boolChangeClass = false;
                 }
+
+                initial = this.pageYOffset;
                 scope.$apply();
+
             });
         };
     });
