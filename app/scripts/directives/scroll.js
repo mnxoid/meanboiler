a3de80efdbb0ce23f5debff89d6fb521e4eb2b2f
Danylo Mylian <danylo.ukl@gmail.com>
2015-05-10 18:29:46 +0300

    Even more improvements to the directive

--- app/scripts/directives/scroll.js
+++ app/scripts/directives/scroll.js
@@ -6,23 +6,30 @@ angular
             // initial offset value; stores precedent offset when compared with current
             var prior = 0;
             // threshold when scrolling up
-            var threshold = 0;
+            var threshold_up = 0;
+            var threshold_down = 0;
+            var threshold_up_limit = 10;
+            var threshold_down_limit = 10;
 
             angular.element($window).bind("scroll", function() {
 
                 var current = this.pageYOffset;
 
-                if (current === 0) {
+                if (!current) {
                     $scope.shrink_nav = false;
                 } else if (current >= prior) {
-                    threshold = 0;
-                    $scope.shrink_nav = true;
+                    threshold_up = 0;
+                    threshold_down++;
+                    if (threshold_down >= threshold_down_limit) {
+                        $scope.shrink_nav = true;
+                        threshold_down = 0;
+                    }
                 } else {
-                    threshold++;
-                    console.log(threshold);
-                    if (threshold >= 10) {
+                    threshold_down = 0;
+                    threshold_up++;
+                    if (threshold_up >= threshold_up_limit) {
                         $scope.shrink_nav = false;
-                        threshold = 0;
+                        threshold_up = 0;
                     }
                 }
 
